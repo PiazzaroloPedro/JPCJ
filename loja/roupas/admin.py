59e17feef2deb98ea7fa9ddbc89b96b1833a8724
PiazzaroloPedro <piazzarolopedro@gmail.com>
2024-03-19 16:53:48 -0300

    u

--- loja/roupas/admin.py
+++ loja/roupas/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-#from roupas.models import Categoria
-#from roupas.models import Marca
+from roupas.models import Categoria
+from roupas.models import Marca
 from roupas.models import Produto
 
-#class CartegoriaAdmin(admin.ModelAdmin):
- #   list_display = ("nome","slug")
-  #  search_fields = ("nome",)
+class CartegoriaAdmin(admin.ModelAdmin):
+   list_display = ("nome","slug")
+   search_fields = ("nome",)
 
-#admin.site.register(CartegoriaAdmin)
+admin.site.register(Categoria)
+
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome','descricao','preco','marca','disponivel'"criado_em")
     search_fields = ('nome',)
 
 admin.site.register(Produto)
+
+
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('nome')
+    search_fields = ('nome',)
+
+admin.site.register(Marca)
