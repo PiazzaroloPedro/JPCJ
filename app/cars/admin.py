61dc8a0226e362f5df59e69c968b667cb3bb088a
PiazzaroloPedro <piazzarolopedro@gmail.com>
2024-03-19 14:27:38 -0300

    update

---
+++ app/cars/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+#from cars.models import car
+
+#class CarAdmin(admin):
+ #   list_dispaly = ("model","brand","factory_year","model_year","value")
+  #  search_fields = ("model",)
+
+#admin.site.register(car, CarAdmin)
