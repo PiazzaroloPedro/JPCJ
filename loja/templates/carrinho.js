cb6a915a201d0e04bd1ca5666c0bf8cbc7e7f963
PiazzaroloPedro <piazzarolopedro@gmail.com>
2024-04-26 08:25:03 -0300

    update
    arrumando o carrinho

--- loja/templates/carrinho.js
+++ loja/templates/carrinho.js
@@ -1,26 +1,40 @@
 // Função para exibir os produtos no carrinho
 function exibirProdutosNoCarrinho() {
-    const carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || []; // Recupera o carrinho da sessionStorage ou cria um novo array vazio se não existir
+    const carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
     const carrinhoSection = document.getElementById('carrinho');
 
     // Limpa o conteúdo atual do carrinho
     carrinhoSection.innerHTML = '';
 
-    // Adiciona cada produto ao carrinho
-    carrinho.forEach(produto => {
-        const divProduto = document.createElement('div');
-        divProduto.innerHTML = `
-            <div>
-                <img src="${produto.imagemURL}">
-                <p>${produto.descricao}</p>
-            </div>
-        `;
-        carrinhoSection.appendChild(divProduto);
+    // Adiciona os produtos ao carrinho em linhas de até 4 produtos por linha
+    for (let i = 0; i < carrinho.length; i += 4) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = i; j < i + 4 && j < carrinho.length; j++) {
+            const divProduto = document.createElement('div');
+            divProduto.classList.add('produto');
+            divProduto.innerHTML = `
+                <div>
+                    <img src="${carrinho[j].imagemURL}">
+                    <p>${carrinho[j].descricao}</p>
+                </div>
+            `;
+            row.appendChild(divProduto);
+        }
+
+        carrinhoSection.appendChild(row);
+    }
+
+    // Adiciona o event listener para o botão "Limpar Carrinho"
+    const clearCartButton = document.querySelector('.clear-cart');
+    clearCartButton.addEventListener('click', function () {
+        // Limpa o carrinho na sessionStorage
+        sessionStorage.removeItem('carrinho');
+        // Reexibe os produtos no carrinho (que estarão vazios após limpar)
+        exibirProdutosNoCarrinho();
     });
 }
 
 // Chama a função ao carregar a página
 window.onload = exibirProdutosNoCarrinho;
-
-
-
